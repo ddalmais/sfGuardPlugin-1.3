1180ee52c0ab1316f12b9aca540af80e5cacbe16
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-02-27 18:50:43 +0000

    sfGuardPlugin: fix routes for 1.1

--- config/config.php
+++ config/config.php
@@ -2,10 +2,5 @@
 
 if (sfConfig::get('app_sf_guard_plugin_routes_register', true) && in_array('sfGuardAuth', sfConfig::get('sf_enabled_modules', array())))
 {
-  $r = sfRouting::getInstance();
-
-  // preprend our routes
-  $r->prependRoute('sf_guard_signin', '/login', array('module' => 'sfGuardAuth', 'action' => 'signin'));
-  $r->prependRoute('sf_guard_signout', '/logout', array('module' => 'sfGuardAuth', 'action' => 'signout'));
-  $r->prependRoute('sf_guard_password', '/request_password', array('module' => 'sfGuardAuth', 'action' => 'password'));
+  $this->dispatcher->connect('routing.load_configuration', array('sfGuardRouting', 'listenToRoutingLoadConfigurationEvent'));
 }
