54967b9f3e375bfe3a939f5585c59866368bb436
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-03-19 18:07:11 +0000

    sfGuardPlugin: fixed support for passwords set to 0 (closes #2862)

--- lib/model/plugin/PluginsfGuardUser.php
+++ lib/model/plugin/PluginsfGuardUser.php
@@ -30,7 +30,7 @@ class PluginsfGuardUser extends BasesfGuardUser
 
   public function setPassword($password)
   {
-    if (!$password)
+    if (!$password) && 0 == strlen($password))
     {
       return;
     }
